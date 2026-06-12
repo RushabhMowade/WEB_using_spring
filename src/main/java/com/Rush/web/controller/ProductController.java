2d7984b12e2ee45d9b36db04644420abb0ca08c7
Rushabh Mowade <rushabhmwd@gmail.com>
2026-06-12 16:09:52 +0530

    update method

--- src/main/java/com/Rush/web/controller/ProductController.java
+++ src/main/java/com/Rush/web/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.Rush.web.controller;
 import com.Rush.web.model.Product;
 import com.Rush.web.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
@@ -16,5 +15,20 @@ public class ProductController {
     @RequestMapping("/Products")
     public List<Product> getProducts(){
         return service.getProducts();
+    }
+
+    @RequestMapping("/Products/{id}")
+    public Product getProductById(@PathVariable int id){
+
+        return service.getProductById(id);
+    }
+    @PostMapping("/addProd")
+    public void addProduct(@RequestBody Product prod){
+        service.addProduct(prod);
+
+    }
+    @PutMapping("/updateProd")
+    public void updateProd(@RequestBody Product prod){
+        service.updateProd(prod);
     }
 }
--- src/main/java/com/Rush/web/service/ProductService.java
+++ src/main/java/com/Rush/web/service/ProductService.java
@@ -3,16 +3,40 @@ package com.Rush.web.service;
 import com.Rush.web.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 @Service
 public class ProductService {
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101,"Iphone",60000),
             new Product(102,"Xiaomi",70000),
-            new Product(103,"Samsung",80000));
+            new Product(103,"Samsung",80000)));
     public List<Product> getProducts(){
 
         return products;
+    }
+    public Product getProductById(int id){
+        return products.stream().filter(p -> p.getProdId() == id)
+                .findFirst().get();
+        //products.stream().filter(p -> p.getProdId() == id).findFirst().orElse(new Product(100,"No item found",0));
+    }
+    public void addProduct(Product prod){
+        products.add(prod);
+    }
+
+    public void updateProd(Product prod){
+        int index=0;
+        for(int i = 0;i< products.size();i++){
+            if(products.get(i).getProdId()==prod.getProdId())
+                index=i;
+        }products.set(index,prod);
+    }
+    public void delProd(int id){
+        int index=0;
+        for(int i = 0;i< products.size();i++){
+            if(products.get(i).getProdId()==id)
+                index=i;
+        }products.remove(index);
     }
 }
